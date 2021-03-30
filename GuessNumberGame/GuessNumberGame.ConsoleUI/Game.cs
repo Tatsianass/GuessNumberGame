01e0ec493467be30a5f74ad517f8054e814c0223
Tatsianass <tanyasnapkova395@gmail.com>
2021-03-30 21:36:44 +0300

    Add main game logic

---
+++ GuessNumberGame/GuessNumberGame.ConsoleUI/Game.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace GuessNumberGame.ConsoleUI
+{
+    public class Game
+    {
+        private static readonly int numberOfTry = 3;
+        private static int wishNumber = 0;
+
+        private static int AskUserWishNumber()
+        {
+            Console.Write("Your try:");
+            return GetEnteredNumber();
+        }
+
+        private static int GetEnteredNumber()
+        {
+            string tryValue = Console.ReadLine();
+            int parcedValue = int.Parse(tryValue);
+            return parcedValue;
+        }
+
+        private static void DisplayResult(bool isUserWon)
+        {
+            if (isUserWon)
+            {
+                Console.WriteLine("Well Done!");
+            }
+            else
+            {
+                Console.WriteLine("Game over!");
+            }
+        }
+
+        private static void DisplayHint(int enteredValue)
+        {
+            if (enteredValue > wishNumber)
+            {
+                Console.WriteLine($"Hint: Wish number is less than {enteredValue}");
+            }
+            else
+            {
+                Console.WriteLine($"Hint: Wish number is bigger than {enteredValue}");
+            }
+        }
+
+        public void Play()
+        {
+            bool isUserWon = false;
+
+            Console.WriteLine("Please, enter wish number");
+            wishNumber = GetEnteredNumber();
+
+            Console.Clear();
+
+            int enteredTry = 0;
+            Console.WriteLine("Guess the number:");
+
+            for (int i = 0; i < numberOfTry; i++)
+            {
+                enteredTry = AskUserWishNumber();
+
+                if (wishNumber == enteredTry)
+                {
+                    isUserWon = true;
+                    break;
+                }
+                else
+                {
+                    DisplayHint(enteredTry);
+                }
+            }
+
+            DisplayResult(isUserWon);
+        }
+    }
+}
---
+++ GuessNumberGame/GuessNumberGame.ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GuessNumberGame.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Game game = new Game();
+                game.Play();
+            } while (true);
+        }
+    }
+}
