9334381fcbdacfbbc661cbb1d700d830ce4d6f73
Tatsianass <tanyasnapkova395@gmail.com>
2021-04-01 00:42:53 +0300

    Add hint about range to user

---
+++ GuessNumberGame/GuessNumberGame.ConsoleUI/Validators/WishNumberValidator.cs
@@ -0,0 +1,25 @@
+﻿using GuessNumberGame.ConsoleUI.Models;
+
+namespace GuessNumberGame.ConsoleUI.Validators
+{
+    public class WishNumberValidator
+    {
+        private readonly GameConfigurationModel gameConfigurationModel;
+        
+        public WishNumberValidator(GameConfigurationModel gameConfigurationModel) 
+        {
+            this.gameConfigurationModel = gameConfigurationModel;
+        }
+
+        public bool IsNumberInRange(int number)
+        {
+            bool isNumberInRange = false;
+            if (number >= gameConfigurationModel.RangeNumberFrom && number <= gameConfigurationModel.RangeNumberTo)
+            {
+                isNumberInRange = true;
+            }
+
+            return isNumberInRange;
+        }
+    }
+}
